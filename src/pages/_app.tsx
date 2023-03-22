166f6661c0311b86f8c490706a54b9bd4442f687
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-03-22 18:33:18 +0200

    chore: borken code before app dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,48 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import * as fs from "fs";
+import * as path from "path";
+import Link from "next/link";
+import { InferGetStaticPropsType } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const getStaticProps = () => {
+  console.log(5555)
+  const posts = fs
+    .readdirSync(path.join(process.cwd(), `src`, `pages`, `posts`))
+    .map((name) => path.parse(name).name);
+  return {
+    appProps: {
+      posts,
+    },
+  };
+};
+
+type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function App({
+  Component,
+  pageProps,
+  appProps
+}: AppProps & { appProps: HomeProps }) {
+  const { posts } = appProps;
+  return (
+    <>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <aside>
+        <h2>Posts</h2>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          {posts.map((name) => (
+            <li key={name}>
+              <Link href={`/posts/${name}`}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,6 @@
 import Head from "next/head"
-import * as fs from "fs"
-import * as path from "path"
-import {InferGetStaticPropsType} from "next"
-import Link from "next/link"
 
-export const getStaticProps = () => {
-  const posts = fs.readdirSync(path.join(process.cwd(), `src`, `pages`, `posts`)).map(
-    name => path.parse(name).name
-  )
-  return {
-    props: {
-      posts
-    }
-  }
-}
-
-type HomeProps = InferGetStaticPropsType<typeof getStaticProps>
-
-export default function Home({ posts }: HomeProps) {
+export default function Home() {
   return (
     <>
       <Head>
@@ -27,11 +10,6 @@ export default function Home({ posts }: HomeProps) {
       </Head>
       <main>
         <h1>Blog</h1>
-        <ul>{posts.map(name =>(
-          <li key={name}>
-            <Link href={`/posts/${name}`}>{name}</Link>
-          </li>
-        ))}</ul>
       </main>
     </>
   )
