a94ea58cf776757804c7868c32ee7dd7947012b5
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-04-13 18:10:07 +0300

    add author profile links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,32 @@ const Nav: React.FC<{ posts: readonly string[] }> = ({ posts }) => (
 );
 
 export default async function Layout({ children }: React.PropsWithChildren) {
-  const posts = await getAllPosts()
+  const posts = await getAllPosts();
   return (
     <html lang="en">
       <body>
-<main>
-      <h1>My detailed and mostly wrong opinions on programming</h1>
-          {children}</main>
-          <aside>
-            <h2>Posts</h2>
-            <Nav posts={posts.map(p => p.id)} />
-          </aside>
+        <main>
+          <h1>My detailed and mostly wrong opinions on programming</h1>
+          {children}
+        </main>
+        <aside>
+          <h2>Posts</h2>
+          <Nav posts={posts.map((p) => p.id)} />
+          <h2>Author</h2>
+          <ol>
+            <li>
+              <Link href="https://github.com/KonstantinSimeonov">Github</Link>
+            </li>
+            <li>
+              <Link href="https://konsimeonov.lol">Website</Link>
+            </li>
+            <li>
+              <Link href="https://www.hackerrank.com/kon_trombon">
+                Hackerrank
+              </Link>
+            </li>
+          </ol>
+        </aside>
       </body>
     </html>
   );
